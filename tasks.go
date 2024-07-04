afe5f1762d16a86e105d02bac64a21e073d00759
hnsia <44498178+hnsia@users.noreply.github.com>
2024-07-05 01:06:44 +0800

    feat: add create task handler

--- tasks.go
+++ tasks.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+var errNameRequired = errors.New("name is required")
+var errProjectIDRequired = errors.New("project id is required")
+var errUserIDRequired = errors.New("user id is required")
 
 type TasksService struct {
 	store Store
@@ -20,9 +27,42 @@ func (s *TasksService) RegisterRoutes(r *mux.Router) {
 }
 
 func (s *TasksService) handleCreateTask(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
 
+	defer r.Body.Close()
+
+	var task *Task
+	err = json.Unmarshal(body, &task)
+	if err != nil {
+		return
+	}
+
+	if err := validateTaskPayload(task); err != nil {
+		return
+	}
+
+	// t, err := s.store.CreateTask
 }
 
 func (s *TasksService) handleGetTask(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func validateTaskPayload(task *Task) error {
+	if task.Name == "" {
+		return errNameRequired
+	}
+
+	if task.ProjectID == 0 {
+		return errProjectIDRequired
+	}
+
+	if task.AssignedToID == 0 {
+		return errUserIDRequired
+	}
+
+	return nil
 }
