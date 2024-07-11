6d2532e88b3d0a8daa217bca0c03072787ca885f
hnsia <44498178+hnsia@users.noreply.github.com>
2024-07-12 01:23:43 +0800

    test: add task tests

--- tasks_test.go
+++ tasks_test.go
@@ -40,4 +40,33 @@ func TestCreateTask(t *testing.T) {
 			t.Error("invalid status code, it should fail")
 		}
 	})
+
+	t.Run("should create a task", func(t *testing.T) {
+		payload := &Task{
+			Name:         "Creating a Test Task",
+			ProjectID:    1,
+			AssignedToID: 42,
+		}
+
+		b, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/tasks", bytes.NewBuffer(b))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/tasks", service.handleCreateTask)
+
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusCreated {
+			t.Errorf("expected status code %d, got %d", http.StatusCreated, rr.Code)
+		}
+	})
 }
